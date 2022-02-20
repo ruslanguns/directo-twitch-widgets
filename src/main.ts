26373afa3b46ad4683c96a2cb4d3e2fce171dcfe
Ruslan Gonzalez <ruslanguns@gmail.com>
2022-02-20 17:43:09 +0000

    Configuracion basica del repositorio

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const port = parseInt(process.env.PORT, 10) || 3000;
+
+  app.setGlobalPrefix('api');
+  app.enableCors();
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      forbidUnknownValues: true,
+    }),
+  );
+
+  await app.listen(port);
+
+  Logger.log(`Nuestra APP esta corriendo en: ${await app.getUrl()}`);
 }
 bootstrap();
