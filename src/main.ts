4fe0fac21d8a420019aabe7d2b5d9c8ec176ff84
Ruslan <ruslanguns@gmail.com>
2022-12-20 19:15:41 +0000

    feat: manage single source of truth for envs

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const port = parseInt(process.env.PORT, 10) || 3000;
+  const port = parseInt(process.env.APP_PORT, 10) || 3000;
 
   app.setGlobalPrefix('api');
   app.enableCors();
